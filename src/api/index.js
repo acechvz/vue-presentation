314f9105e5111780cb524eef506a786af20940bb
Alan <acechvz@gmail.com>
2021-08-23 22:23:10 -0500

    initial commit

--- src/api/index.js
+++ src/api/index.js
@@ -6,15 +6,4 @@ const instance = axios.create({
   baseURL: API_BASE_URL,
 });
 
-function testFunction() {
-  let a = 1;
-  if (true) {
-    a = 2;
-  } else {
-    a = 3;
-  }
-}
-
-testFunction();
-
 export default instance;
